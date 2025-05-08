b1c8352c7370066a85b46bce9f928aa422e60dbd
anthony-melin <anthony.melin@laposte.net>
2025-05-09 00:13:39 +0200

    :white_check_mark: starships

--- src/components/Starships.tsx
+++ src/components/Starships.tsx
@@ -5,12 +5,23 @@ type StarshipsProps = PropsWithChildren<{ values: string[] }>
 const Starships = ({ values, ...restProps }: StarshipsProps) => {
     return (
         <div data-testid="starships" {...restProps}>
-            <p>Starships</p>
-            <ul>
-                {values.map((value, index) => {
-                    return <li key={`starship-${index}`} data-testid={`starship-${index}`}>{value}</li>
-                })}
-            </ul>
+            <p data-testid="starships-title">Starships</p>
+            {values.length === 0 ? (
+                <p data-testid="starships-empty">No starships linked</p>
+            ) : (
+                <ul>
+                    {values.map((value, index) => {
+                        return (
+                            <li
+                                key={`starships-${index}`}
+                                data-testid={`starships-${index}`}
+                            >
+                                {value}
+                            </li>
+                        )
+                    })}
+                </ul>
+            )}
         </div>
     )
 }
