4cb3d0ba9109630b668eb03c2c667a94dde32146
anthony-melin <anthony.melin@laposte.net>
2025-05-09 01:54:16 +0200

    :recycle: factory of component: list

--- src/components/Films.tsx
+++ src/components/Films.tsx
@@ -1,29 +1,3 @@
-import { PropsWithChildren } from 'react'
+import ListValueFactory from "./__components__/ListValueFactory.tsx";
 
-type FilmsProps = PropsWithChildren<{ values: string[] }>
-
-const Films = ({ values, ...restProps }: FilmsProps) => {
-    return (
-        <div data-testid="films" {...restProps}>
-            <p data-testid="films-title">Films</p>
-            {values.length === 0 ? (
-                <p data-testid="films-empty">No films linked</p>
-            ) : (
-                <ul>
-                    {values.map((value, index) => {
-                        return (
-                            <li
-                                key={`films-${index}`}
-                                data-testid={`films-${index}`}
-                            >
-                                {value}
-                            </li>
-                        )
-                    })}
-                </ul>
-            )}
-        </div>
-    )
-}
-
-export default Films
+export default ListValueFactory('films', 'Films', 'films')
--- src/components/Starships.tsx
+++ src/components/Starships.tsx
@@ -1,29 +1,3 @@
-import { PropsWithChildren } from 'react'
+import ListValueFactory from "./__components__/ListValueFactory.tsx";
 
-type StarshipsProps = PropsWithChildren<{ values: string[] }>
-
-const Starships = ({ values, ...restProps }: StarshipsProps) => {
-    return (
-        <div data-testid="starships" {...restProps}>
-            <p data-testid="starships-title">Starships</p>
-            {values.length === 0 ? (
-                <p data-testid="starships-empty">No starships linked</p>
-            ) : (
-                <ul>
-                    {values.map((value, index) => {
-                        return (
-                            <li
-                                key={`starships-${index}`}
-                                data-testid={`starships-${index}`}
-                            >
-                                {value}
-                            </li>
-                        )
-                    })}
-                </ul>
-            )}
-        </div>
-    )
-}
-
-export default Starships
+export default ListValueFactory('starships', 'Starships', 'starships')
