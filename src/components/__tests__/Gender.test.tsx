3e491c5cbdeda1e85a3a925376d42a6a9b7254f2
anthony-melin <anthony.melin@laposte.net>
2025-05-08 23:19:48 +0200

    :white_check_mark: gender

---
+++ src/components/__tests__/Gender.test.tsx
@@ -0,0 +1,19 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Gender from '../Gender.tsx'
+
+describe('Gender', () => {
+    beforeEach(() => {
+        render(<Gender value="VALUE" />)
+    })
+
+    it('should match snapshot', () => {
+        expect(screen.getByTestId('gender')).toMatchSnapshot()
+    })
+
+    it('should have text', () => {
+        expect(screen.getByTestId('gender')).toHaveTextContent(
+            'Gender: VALUE'
+        )
+    })
+})
