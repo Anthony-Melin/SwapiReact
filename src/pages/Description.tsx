81b534800b9b95a61cfb0c08ef00e2b74d9a4fb5
anthony-melin <anthony.melin@laposte.net>
2025-05-08 13:56:37 +0200

    :sparkles: Height component

--- src/pages/Description.tsx
+++ src/pages/Description.tsx
@@ -1,6 +1,7 @@
 import { ClientLoaderFunctionArgs, useLoaderData } from 'react-router'
 import getFetch from '@api/getFetch.ts'
 import Details from '@api/types/details.ts'
+import Height from "../components/Height.tsx";
 
 export const clientLoader = async ({ request }: ClientLoaderFunctionArgs) => {
     const url = new URL(request.url)
@@ -18,11 +19,15 @@ const Description = () => {
             <h3>{name || title}</h3>
             {Object.entries(restKeys).map(([key, value]) => {
                 if (typeof value === 'string') {
-                    return (
-                        <p>
-                            {key}: {value}
-                        </p>
-                    )
+                    if (key === 'height') {
+                        return <Height value={value} />
+                    } else {
+                        return (
+                            <p>
+                                {key}: {value}
+                            </p>
+                        )
+                    }
                 }
             })}
         </main>
