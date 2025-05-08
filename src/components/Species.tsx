38c1511a06dbc2e6f8227ef406c45e11bbcabf09
anthony-melin <anthony.melin@laposte.net>
2025-05-09 01:54:16 +0200

    :recycle: factory of component: list

--- src/components/Species.tsx
+++ src/components/Species.tsx
@@ -1,29 +1,3 @@
-import { PropsWithChildren } from 'react'
+import ListValueFactory from "./__components__/ListValueFactory.tsx";
 
-type SpeciesProps = PropsWithChildren<{ values: string[] }>
-
-const Species = ({ values, ...restProps }: SpeciesProps) => {
-    return (
-        <div data-testid="species" {...restProps}>
-            <p data-testid="species-title">Species</p>
-            {values.length === 0 ? (
-                <p data-testid="species-empty">No species linked</p>
-            ) : (
-                <ul>
-                    {values.map((value, index) => {
-                        return (
-                            <li
-                                key={`species-${index}`}
-                                data-testid={`species-${index}`}
-                            >
-                                {value}
-                            </li>
-                        )
-                    })}
-                </ul>
-            )}
-        </div>
-    )
-}
-
-export default Species
+export default ListValueFactory('species', 'Species', 'species')
--- src/components/Vehicules.tsx
+++ src/components/Vehicules.tsx
@@ -1,29 +1,3 @@
-import { PropsWithChildren } from 'react'
+import ListValueFactory from "./__components__/ListValueFactory.tsx";
 
-type VehiculesProps = PropsWithChildren<{ values: string[] }>
-
-const Vehicules = ({ values, ...restProps }: VehiculesProps) => {
-    return (
-        <div data-testid="vehicules" {...restProps}>
-            <p data-testid="vehicules-title">Vehicules</p>
-            {values.length === 0 ? (
-                <p data-testid="vehicules-empty">No vehicules linked</p>
-            ) : (
-                <ul>
-                    {values.map((value, index) => {
-                        return (
-                            <li
-                                key={`vehicules-${index}`}
-                                data-testid={`vehicules-${index}`}
-                            >
-                                {value}
-                            </li>
-                        )
-                    })}
-                </ul>
-            )}
-        </div>
-    )
-}
-
-export default Vehicules
+export default ListValueFactory('vehicules', 'Vehicules', 'vehicules')
