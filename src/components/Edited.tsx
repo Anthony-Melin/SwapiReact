e9db198c3db9ad2e93a63f62ddec271c82042400
anthony-melin <anthony.melin@laposte.net>
2025-05-09 01:03:47 +0200

    :white_check_mark: edited

--- src/components/Edited.tsx
+++ src/components/Edited.tsx
@@ -3,9 +3,10 @@ import { PropsWithChildren } from 'react'
 type EditedProps = PropsWithChildren<{ value: string }>
 
 const Edited = ({ value, ...restProps }: EditedProps) => {
+    const date = new Date(value)
     return (
         <p data-testid="edited" {...restProps}>
-            Edited: {value}
+            Edited: {date.toLocaleString('fr-FR')}
         </p>
     )
 }
