9e2437629bae49f5583656bbddcd0f4349ff1265
anthony-melin <anthony.melin@laposte.net>
2025-05-09 00:06:43 +0200

    :white_check_mark: species

--- src/components/Species.tsx
+++ src/components/Species.tsx
@@ -5,12 +5,23 @@ type SpeciesProps = PropsWithChildren<{ values: string[] }>
 const Species = ({ values, ...restProps }: SpeciesProps) => {
     return (
         <div data-testid="species" {...restProps}>
-            <p>Species</p>
-            <ul>
-                {values.map((value, index) => {
-                    return <li key={`species-${index}`} data-testid={`species-${index}`}>{value}</li>
-                })}
-            </ul>
+            <p data-testid="species-title">Species</p>
+            {values.length === 0 ? (
+                <p data-testid="species-empty">No species linked</p>
+            ) : (
+                <ul>
+                    {values.map((value, index) => {
+                        return (
+                            <li
+                                key={`species-${index}`}
+                                data-testid={`species-${index}`}
+                            >
+                                {value}
+                            </li>
+                        )
+                    })}
+                </ul>
+            )}
         </div>
     )
 }
