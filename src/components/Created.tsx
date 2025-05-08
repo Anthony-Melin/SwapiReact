f2050128fa36b2f607070d9de6f4c609fa9cb9ce
anthony-melin <anthony.melin@laposte.net>
2025-05-09 01:02:15 +0200

    :white_check_mark: created

--- src/components/Created.tsx
+++ src/components/Created.tsx
@@ -3,9 +3,10 @@ import { PropsWithChildren } from 'react'
 type CreatedProps = PropsWithChildren<{ value: string }>
 
 const Created = ({ value, ...restProps }: CreatedProps) => {
+    const date = new Date(value)
     return (
         <p data-testid="created" {...restProps}>
-            Created: {value}
+            Created: {date.toLocaleString('fr-FR')}
         </p>
     )
 }
