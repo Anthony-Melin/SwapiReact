fe90c6dca93e6b991a8f5b2d47de5eff9f5a4ced
anthony-melin <anthony.melin@laposte.net>
2025-05-11 19:04:43 +0200

    :recycle: simplify components

--- src/pages/Description.tsx
+++ src/pages/Description.tsx
@@ -1,25 +1,9 @@
 import { ClientLoaderFunctionArgs, useLoaderData } from 'react-router'
 import getFetch from '@api/getFetch.ts'
 import Details from '@api/types/details.ts'
-import Height from '../components/Height.tsx'
-import Mass from '../components/Mass.tsx'
-import HairColor from '../components/HairColor.tsx'
-import SkinColor from '../components/SkinColor.tsx'
-import EyeColor from '../components/EyeColor.tsx'
-import BirthYear from '../components/BirthYear.tsx'
-import Gender from '../components/Gender.tsx'
-import HomeWorld from '../components/HomeWorld.tsx'
-import Created from '../components/Created.tsx'
-import Edited from '../components/Edited.tsx'
-import Films from '../components/Films.tsx'
-import Species from '../components/Species.tsx'
-import Vehicles from '../components/Vehicles.tsx'
-import Starships from '../components/Starships.tsx'
-import Characters from '../components/Characters.tsx'
-import ReleaseDate from '../components/ReleaseDate.tsx'
-import Producer from '../components/Producer.tsx'
-import Director from '../components/Director.tsx'
-import Episode from '../components/Episode.tsx'
+import LabelValue from '../components/LabelValue.tsx'
+import LabelDate from '../components/LabelDate.tsx'
+import ListValue from '../components/ListValue.tsx'
 
 export const clientLoader = async ({ request }: ClientLoaderFunctionArgs) => {
     const url = new URL(request.url)
@@ -38,46 +22,184 @@ const Description = () => {
             {Object.entries(restKeys).map(([key, value]) => {
                 if (typeof value === 'string') {
                     if (key === 'height') {
-                        return <Height key={key} value={value} />
+                        return (
+                            <LabelValue
+                                key={key}
+                                label="Height"
+                                value={value}
+                                testid="height"
+                            />
+                        )
                     } else if (key === 'mass') {
-                        return <Mass key={key} value={value} />
+                        return (
+                            <LabelValue
+                                key={key}
+                                label="Mass"
+                                value={value}
+                                testid="mass"
+                            />
+                        )
                     } else if (key === 'hair_color') {
-                        return <HairColor key={key} value={value} />
+                        return (
+                            <LabelValue
+                                key={key}
+                                label="Hair color"
+                                value={value}
+                                testid="haircolor"
+                            />
+                        )
                     } else if (key === 'skin_color') {
-                        return <SkinColor key={key} value={value} />
+                        return (
+                            <LabelValue
+                                key={key}
+                                label="Skin color"
+                                value={value}
+                                testid="skincolor"
+                            />
+                        )
                     } else if (key === 'eye_color') {
-                        return <EyeColor key={key} value={value} />
+                        return (
+                            <LabelValue
+                                key={key}
+                                label="Eye color"
+                                value={value}
+                                testid="eyecolor"
+                            />
+                        )
                     } else if (key === 'birth_year') {
-                        return <BirthYear key={key} value={value} />
+                        return (
+                            <LabelValue
+                                key={key}
+                                label="Birth year"
+                                value={value}
+                                testid="birthyear"
+                            />
+                        )
                     } else if (key === 'gender') {
-                        return <Gender key={key} value={value} />
+                        return (
+                            <LabelValue
+                                key={key}
+                                label="Gender"
+                                value={value}
+                                testid="gender"
+                            />
+                        )
                     } else if (key === 'homeworld') {
-                        return <HomeWorld key={key} value={value} />
+                        return (
+                            <LabelValue
+                                key={key}
+                                label="Home world"
+                                value={value}
+                                testid="homeworld"
+                            />
+                        )
                     } else if (key === 'created') {
-                        return <Created key={key} value={value} />
+                        return (
+                            <LabelDate
+                                key={key}
+                                label="Created"
+                                value={value}
+                                testid="created"
+                            />
+                        )
                     } else if (key === 'edited') {
-                        return <Edited key={key} value={value} />
+                        return (
+                            <LabelDate
+                                key={key}
+                                label="Edited"
+                                value={value}
+                                testid="edited"
+                            />
+                        )
                     } else if (key === 'release_date') {
-                        return <ReleaseDate key={key} value={value} />
+                        return (
+                            <LabelValue
+                                key={key}
+                                label="Release date"
+                                value={value}
+                                testid="releasedate"
+                            />
+                        )
                     } else if (key === 'producer') {
-                        return <Producer key={key} value={value} />
+                        return (
+                            <LabelValue
+                                key={key}
+                                label="Producer"
+                                value={value}
+                                testid="producer"
+                            />
+                        )
                     } else if (key === 'director') {
-                        return <Director key={key} value={value} />
+                        return (
+                            <LabelValue
+                                key={key}
+                                label="Director"
+                                value={value}
+                                testid="director"
+                            />
+                        )
                     }
                 } else if (typeof value === 'number') {
-                    return <Episode key={key} value={value.toString()} />
+                    return (
+                        <LabelValue
+                            key={key}
+                            label="Episode"
+                            value={value.toString()}
+                            testid="episode"
+                        />
+                    )
                 } else if (value instanceof Array) {
                     const values = value.filter((v) => typeof v === 'string')
                     if (key === 'films') {
-                        return <Films key={key} values={values} />
+                        return (
+                            <ListValue
+                                key={key}
+                                title="Films"
+                                item="film"
+                                values={values}
+                                testid="film"
+                            />
+                        )
                     } else if (key === 'species') {
-                        return <Species key={key} values={values} />
+                        return (
+                            <ListValue
+                                key={key}
+                                title="Species"
+                                item="species"
+                                values={values}
+                                testid="species"
+                            />
+                        )
                     } else if (key === 'vehicles') {
-                        return <Vehicles key={key} values={values} />
+                        return (
+                            <ListValue
+                                key={key}
+                                title="Vehicles"
+                                item="vehicle"
+                                values={values}
+                                testid="vehicle"
+                            />
+                        )
                     } else if (key === 'starships') {
-                        return <Starships key={key} values={values} />
+                        return (
+                            <ListValue
+                                key={key}
+                                title="Starships"
+                                item="starship"
+                                values={values}
+                                testid="species"
+                            />
+                        )
                     } else if (key === 'characters') {
-                        return <Characters key={key} values={values} />
+                        return (
+                            <ListValue
+                                key={key}
+                                title="Characters"
+                                item="character"
+                                values={values}
+                                testid="character"
+                            />
+                        )
                     }
                 }
             })}
