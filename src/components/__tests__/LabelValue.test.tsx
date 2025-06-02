b5496915bcf62074c4cafd6796c2b8faa4ad298d
anthony-melin <anthony.melin@laposte.net>
2025-06-02 17:54:32 +0200

    ✨ LabelValue component: remap value

--- src/components/__tests__/LabelValue.test.tsx
+++ src/components/__tests__/LabelValue.test.tsx
@@ -1,17 +1,21 @@
-import { describe, it, expect, beforeEach } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import LabelValue from '../LabelValue.tsx'
 
 describe('LabelValue', () => {
-    beforeEach(() => {
-        render(<LabelValue label="LABEL" value="VALUE" testid="testid" />)
-    })
 
     it('should match snapshot', () => {
+        render(<LabelValue label="LABEL" value="VALUE" testid="testid" />)
         expect(screen.getByTestId('testid')).toMatchSnapshot()
     })
 
     it('should render text', () => {
+        render(<LabelValue label="LABEL" value="VALUE" testid="testid" />)
         expect(screen.getByTestId('testid')).toHaveTextContent('LABEL: VALUE')
+    })
+
+    it('should render remaped value', () => {
+        render(<LabelValue label="LABEL" value="VALUE" remap={{'VALUE': 'REMAP_VALUE'}} testid="testid" />)
+        expect(screen.getByTestId('testid')).toHaveTextContent('LABEL: REMAP_VALUE')
     })
 })
