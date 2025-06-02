b010f95f10abbe01f68ea155d0cefba11dc0b688
anthony-melin <anthony.melin@laposte.net>
2025-06-02 17:54:32 +0200

    ✨ LabelValue component: remap value

--- src/components/LabelValue.tsx
+++ src/components/LabelValue.tsx
@@ -4,17 +4,20 @@ type LabelValueProps = PropsWithChildren<{
     label: string
     value: string
     testid: string
+    remap?: Record<string, string>
 }>
 
 const LabelValue = ({
     label,
     value,
     testid,
+    remap = {},
     ...restProps
 }: LabelValueProps) => {
+    const displayedValue = value in remap ? remap[value] : value
     return (
         <p data-testid={testid} {...restProps}>
-            {label}: {value}
+            {label}: {displayedValue}
         </p>
     )
 }
