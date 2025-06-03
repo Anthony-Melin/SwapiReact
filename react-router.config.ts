fe9a71edca2ec4902af6eb8207fa5ee4b582fe20
anthony-melin <anthony.melin@laposte.net>
2025-06-03 14:18:03 +0200

    🔧 define base url

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from '@react-router/dev/dist/config'
+import { loadEnv } from 'vite'
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd())
 
 export default {
     ssr: false,
     appDirectory: './src',
+    basename: env.VITE_BASE_URL,
 } satisfies Config
