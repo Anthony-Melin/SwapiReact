ebc0c091bc98412a9a15077960de2e98a97e8921
anthony-melin <anthony.melin@laposte.net>
2025-06-04 10:05:01 +0200

    🔨 alias for assets

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig(({ mode }) => {
         resolve: {
             alias: {
                 '@api': path.resolve(__dirname, './src/api'),
+                '@assets': path.resolve(__dirname, './src/assets'),
                 '@components': path.resolve(__dirname, './src/components'),
                 '@hooks': path.resolve(__dirname, './src/hooks'),
                 '@pages': path.resolve(__dirname, './src/pages'),
