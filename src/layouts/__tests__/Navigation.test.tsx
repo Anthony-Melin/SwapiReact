4d522688e49fb37c94b12a82f53da1c7dca95873
anthony-melin <anthony.melin@laposte.net>
2025-08-16 11:56:44 +0200

    🔥 rm skipped test

--- src/layouts/__tests__/Navigation.test.tsx
+++ src/layouts/__tests__/Navigation.test.tsx
@@ -14,35 +14,6 @@ describe('Description', () => {
         },
     ])
 
-    it.skip('should render navigation buttons', async () => {
-        render(<Stub />)
-
-        let element
-        element = await screen.findByTestId('section-0')
-        expect(element).toHaveTextContent('people')
-        expect(element).toHaveAttribute('href', '/people')
-
-        element = await screen.findByTestId('section-1')
-        expect(element).toHaveTextContent('films')
-        expect(element).toHaveAttribute('href', '/films')
-
-        element = await screen.findByTestId('section-2')
-        expect(element).toHaveTextContent('planets')
-        expect(element).toHaveAttribute('href', '/planets')
-
-        element = await screen.findByTestId('section-3')
-        expect(element).toHaveTextContent('species')
-        expect(element).toHaveAttribute('href', '/species')
-
-        element = await screen.findByTestId('section-4')
-        expect(element).toHaveTextContent('starships')
-        expect(element).toHaveAttribute('href', '/starships')
-
-        element = await screen.findByTestId('section-5')
-        expect(element).toHaveTextContent('vehicles')
-        expect(element).toHaveAttribute('href', '/vehicles')
-    })
-
     it('should render navigation buttons', async () => {
         render(
             <SectionContext
